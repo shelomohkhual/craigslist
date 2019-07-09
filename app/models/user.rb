bf2cc868253340a5c013798b0eddf29f8ed77901
Shelo <siankhual96@gmail.com>
2019-07-09 15:18:12 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name, :email
+  # validates :email, uniqueness: {casesensitive: false}
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+end
